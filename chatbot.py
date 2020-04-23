b1e01e596d74a1696fe0c2257a0c108aa8603305
r(0_o)t <50882504+01010000-kumar@users.noreply.github.com>
2020-04-24 01:04:34 +0530

    Update chatbot.py

--- chatbot.py
+++ chatbot.py
@@ -129,10 +129,14 @@ for answer in clean_answers:
             ints.append(answerwords2int[word])
     answers_to_int.append(ints)
 
+#sorted questions and answers by the length of questions
 sorted_clean_questions =  []
 sorted_clean_answers = []
 for length in range(1,25 + 1):
-    
+    for i in enumerate(questions_to_int):
+        if len(i[1]) == length:
+            sorted_clean_questions.append(questions_to_int[i[0]])
+            sorted_clean_answers.append(answers_to_int[i[0]])
 
 
 
