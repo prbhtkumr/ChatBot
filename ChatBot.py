d834b425df2eb018086c6eaa9bd239527bc25fa9
r(0_o)t <50882504+01010000-kumar@users.noreply.github.com>
2020-04-24 03:00:22 +0530

    preprocessing the targets

--- ChatBot.py
+++ ChatBot.py
@@ -144,6 +144,38 @@ def model_inputs():
     keep_prob = tf.placeholder(tf.float32, name = 'keep_prob')
     return inputs, targets, lr, keep_prob
 
+#preprocessing the targets
+def preprocess_targets(targets, word2int, batch_size):
+    left_side = tf.fill([batch_size, 1], word2int['<SOS>'])
+    right_side = tf.strided_slice(target, [0,0], [batch_size, -1], [1,1])
+    preprocessed_targets = tf.concat([left_side, right_side], 1)
+    return preprocessed_targets
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
