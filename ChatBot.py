1006426275280da6c65a2b0c412dd9546dd35258
r(0_o)t <50882504+01010000-kumar@users.noreply.github.com>
2020-04-25 03:36:29 +0530

    Update ChatBot.py

--- ChatBot.py
+++ ChatBot.py
@@ -156,6 +156,11 @@ def encoder_rnn_layer(rnn_inputs, rnn_size, num_layers, keep_prob, sequence_leng
     lstm = tf.contrib.rnn.BasicLSTMCell(rnn_size)
     lstm_dropout = tf.contrib.rnn.DropoutWrapper(lstm, input_keep_prob = keep_prob)
     encoder_cell = tf.contrib.rnn.MultiRNNCell([lstm_dropout] * num_layers)
+    _, encoder_state = tf.nn.bidirectional_dynamic_rnn(cell_fw = encoder_cell,
+                                                       cell_bw = encoder_cell,
+                                                       sequence_length = sequence_length
+                                                       inputs = rnn_inputs,
+                                                       dtype = tf.float32)
 
 
     
