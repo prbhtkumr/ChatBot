debb590b7b2699a2b74586465f046215355d9f22
r(0_o)t <50882504+01010000-kumar@users.noreply.github.com>
2020-04-25 23:49:14 +0530

    Update ChatBot.py

--- ChatBot.py
+++ ChatBot.py
@@ -164,7 +164,7 @@ def encoder_rnn_layer(rnn_inputs, rnn_size, num_layers, keep_prob, sequence_leng
     return encoder_state
 
 #decoding the training set
-def decoder_training_set(encoder_state, decoder_cell, decoder_embedded_input, sequence_length, output_function, keep_prob, batch_size):
+def decoder_training_set(encoder_state, decoder_cell, decoder_embedded_input, sequence_length, decoding_scope,output_function, keep_prob, batch_size):
     attention_states = tf.zeros([batch_size, 1, decoder_cell.output_size])
     attention_keys, attention_values, attention_score_function, attention_construct_function = tf.contrib.seq2seq.prepare_attention(attention_states, attention_option = 'bahdanau', num_units = decoder_cell.output_size)
     training_decoder_function =  tf.contrib.seq2seq.attention_decoder_fn_train(encoder_state[0],
@@ -173,7 +173,14 @@ def decoder_training_set(encoder_state, decoder_cell, decoder_embedded_input, se
                                                                                attention_score_function,
                                                                                attention_construct_function,
                                                                                name = 'attn_dec_train')
-    decoder_output, decoder_final_state, decoder_final_context_state =         
+    decoder_output, decoder_final_state, decoder_final_context_state = tf.contrib.seq2seq.dynamic_rnn_decoder(decoder_cell,
+                                                                                                              training_decoder_function,
+                                                                                                              decoder_embedded_input,
+                                                                                                              sequence_length,
+                                                                                                              scope = decoding_scope)
+    decoder_output_dropout = tf.nn.dropout(decoder_output, keep_prob)
+    return output_function(decoder_output_dropout)
+
 
     
 
